11177163c51946ab48bb369560f183865ac155f8
taisserodio <taisserodio12@gmail.com>
2022-09-08 16:22:31 -0300

    Subi os Exercicios Da A013

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,5 +1,23 @@
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
+switch(nacionalidade) {
+    case "brasileira" :
+        console.log("a pessoa é do Brasil!");
+        break
+    case"Argentina":
+        console.log("a pessoa é da argentina!");
+        break
+    case"Uruguai":
+        console.log("a pessoa é da Uruguai!");
+        break
+    case"Chile":
+        console.log("a pessoa é da Colombia!");
+        break
+    default :
+        console.log("Nacionalidade não encontrada")
+}
+
+/*
 if(nacionalidade === "brasileira"){
     console.log("a pessoa é do Brasil!");
 } else if(nacionalidade === "argentina"){
@@ -12,4 +30,4 @@ if(nacionalidade === "brasileira"){
     console.log("a pessoa é da Colômbia!");
 } else{
     console.log("nacionalidade não encontrada")
-}
+}*/
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,6 @@
 //código a ser reescrito
 
-if(nome === "José"){
+/*if(nome === "José"){
 	console.log("Oi, Zé!");
 } else {
 	console.log("Olá, "+nome);
@@ -10,4 +10,10 @@ if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
-}
+}*/
+
+let nome=prompt("Digite o nome")
+nome === "Jose" ? console.log("Oi,Zé" ): console.log("Voce nao é o Zé");
+
+const idade=18
+idade >=18 ? console.log("Pode tirar carteira de motorista!") : console.log ("Ainda Nao pode tirar a carteira de motorista");
